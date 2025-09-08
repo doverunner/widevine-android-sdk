c599e303606d992f5ed4427e5e7d2804088a8223
sungju Yun <yuyu04@naver.com>
2025-09-08 10:35:45 +0900

    INKA Studio -> DoveRunner name change

--- samples/androidTv/app/src/main/java/com/doverunner/androidtvsample/CardPresenter.java
+++ samples/androidTv/app/src/main/java/com/doverunner/androidtvsample/CardPresenter.java
@@ -73,7 +73,7 @@ public class CardPresenter extends Presenter {
 		Log.d(TAG, "onBindViewHolder");
 		if (movie.getCardImageUrl() != null) {
 			cardView.setTitleText(movie.name);
-			cardView.setContentText("INKA Studio");
+			cardView.setContentText("DoveRunner");
 			cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT);
 			Glide.with(viewHolder.view.getContext())
 					.load(movie.getCardImageUrl())
