6de22e315d5b4106518e21646355a2a55d3515e1
sungju Yun <yuyu04@naver.com>
2025-11-12 10:11:43 +0900

    # Version 4.5.1 - **What’s new**   - The `mimeType` in `ContentData` class parameter has been added     - the parameter can enforce the MIME type for media content.

--- samples/advanced/app/src/main/java/com/doverunner/advencedsample/ObjectSingleton.kt
+++ samples/advanced/app/src/main/java/com/doverunner/advencedsample/ObjectSingleton.kt
@@ -1,6 +1,7 @@
 package com.doverunner.advencedsample
 
 import android.content.Context
+import androidx.media3.common.MimeTypes
 import androidx.media3.exoplayer.offline.DownloadManager
 import androidx.media3.exoplayer.offline.DownloadNotificationHelper
 import androidx.media3.exoplayer.upstream.CmcdConfiguration
@@ -49,6 +50,7 @@ class ObjectSingleton {
             val title: String,
             val contentId: String,
             val url: String,
+            val mimeType: String? = null,
             val drmConfig: DrmConfigration,
             val localFileUrl: String? = null,
         )
@@ -61,10 +63,11 @@ class ObjectSingleton {
 
         val inputs = listOf(
             ContentInput(
-                "basic content",
-                "demo-bbb-simple",
-                "https://drm-contents.doverunner.com/DEMO/app/big_buck_bunny/dash/stream.mpd",
-                DrmConfigration(
+                title = "basic content",
+                contentId = "demo-bbb-simple",
+                url = "https://drm-contents.doverunner.com/DEMO/app/big_buck_bunny/dash/stream.mpd",
+                mimeType = MimeTypes.APPLICATION_MPD,
+                drmConfig = DrmConfigration(
                     siteId = "DEMO",
                     authData = "eyJkcm1fdHlwZSI6IldpZGV2aW5lIiwic2l0ZV9pZCI6IkRFTU8iLCJ1c2VyX2lkIjoidGVzdFVzZXIiLCJjaWQiOiJkZW1vLWJiYi1zaW1wbGUiLCJwb2xpY3kiOiI5V3FJV2tkaHB4VkdLOFBTSVljbkp1dUNXTmlOK240S1ZqaTNpcEhIcDlFcTdITk9uYlh6QS9pdTdSa0Vwbk85c0YrSjR6R000ZkdCMzVnTGVORGNHYWdPY1Q4Ykh5c3k0ZHhSY2hYV2tUcDVLdXFlT0ljVFFzM2E3VXBnVVdTUCIsInJlc3BvbnNlX2Zvcm1hdCI6Im9yaWdpbmFsIiwia2V5X3JvdGF0aW9uIjpmYWxzZSwidGltZXN0YW1wIjoiMjAyMi0wOS0xOVQwNzo0Mjo0MFoiLCJoYXNoIjoiNDBDb1RuNEpFTnpZUHZrT1lTMHkvK2VIN1dHK0ZidUIvcThtR3VoaHVNRT0ifQ"                )
             ),
@@ -79,22 +82,33 @@ class ObjectSingleton {
                 )
             ),
             ContentInput(
-                "hls",
-                "TestRunner_HLS",
-                "https://drm-contents.doverunner.com/TEST/PACKAGED_CONTENT/TEST_SIMPLE/cmaf/master.m3u8",
-                DrmConfigration(
+                title = "hls",
+                contentId = "TestRunner_HLS",
+                url = "https://drm-contents.doverunner.com/TEST/PACKAGED_CONTENT/TEST_SIMPLE/cmaf/master.m3u8",
+                mimeType = MimeTypes.APPLICATION_M3U8,
+                drmConfig = DrmConfigration(
                     "DEMO",
                     "eyJrZXlfcm90YXRpb24iOmZhbHNlLCJyZXNwb25zZV9mb3JtYXQiOiJvcmlnaW5hbCIsInVzZXJfaWQiOiJUZXN0UnVubmVyIiwiZHJtX3R5cGUiOiJ3aWRldmluZSIsInNpdGVfaWQiOiJERU1PIiwiaGFzaCI6IjdqcjNOb2w4N1l1U29hNlk2RXJCMFVoMkNIM1pWR2VBUVNtMTh5YkZheFU9IiwiY2lkIjoiVGVzdFJ1bm5lciIsInBvbGljeSI6IjlXcUlXa2RocHhWR0s4UFNJWWNuSnNjdnVBOXN4Z3ViTHNkK2FqdVwvYm9tUVpQYnFJK3hhZVlmUW9jY2t2dUVmQWFxZFc1aFhnSk5nY1NTM2ZTN284TnNqd3N6ak11dnQrMFF6TGtaVlZObXgwa2VmT2Uyd0NzMlRJVGdkVTRCdk45YWJoZDByUWtNSXJtb0llb0pIcUllSGNSdlZmNlQxNFJtVEFERXBDWTdQSGZQZlwvVkZZXC9WYlh1eFhcL1dUdFZTRkpTSDlzeHB3UUlRWHI1QjZSK0FhYWZTZlZYU0trNG1WRmxlMlBcL3Byamg1OCtiT2hidFU0NDRseDlvcmVHNSIsInRpbWVzdGFtcCI6IjIwMjMtMDUtMTlUMDI6MTM6NTlaIn0"
                 )
             ),
             ContentInput(
-                "licensecipher",
-                "LicenseCipher",
-                "https://drm-contents.doverunner.com/DEMO/app/big_buck_bunny/dash/stream.mpd",
-                DrmConfigration(
+                title = "licensecipher",
+                contentId = "LicenseCipher",
+                url = "https://drm-contents.doverunner.com/DEMO/app/big_buck_bunny/dash/stream.mpd",
+                drmConfig = DrmConfigration(
                     siteId = "DEMO",
                     authData = "eyJrZXlfcm90YXRpb24iOmZhbHNlLCJyZXNwb25zZV9mb3JtYXQiOiJvcmlnaW5hbCIsInVzZXJfaWQiOiJ0ZXN0VXNlciIsImRybV90eXBlIjoid2lkZXZpbmUiLCJzaXRlX2lkIjoiREVNTyIsImhhc2giOiJjQlZFT0RWalI3UjN2cDJzc3VXUVwvc05Pb3RvQWJKZEttcExcLzNlc1k1bmc9IiwiY2lkIjoiZGVtby1iYmItc2ltcGxlIiwicG9saWN5IjoiOVdxSVdrZGhweFZHSzhQU0lZY25Kc2N2dUE5c3hndWJMc2QrYWp1XC9ib2x5QTB6THVJT0VFNll4MlRiWVdCTG9OaGFjU0ViN0F4c0FVQ3JBQ3VTOFVUSzVIaGo1QXZ4VHhUREhsdFRcL0lvdTJ6XC95QUZCZE5CbDVkK1ByYWxXc2djNlRocEhsRFNHWFYwYWx5cG5iR2UyYkZuQ1wvRGZpbHlUTHVFcFdKZXNMdXd1R05RNHhOVndNT0o1WWxcL3pOc1FkMWY1YyttTWFYRXdleEZrUXRVUG5mdUk2WFdDVlBHWjZcL1NoZFBtbzlWMWRubVhoSktVZjhrOWFJOTdzVUV6VVwvSjJUYVpTYVU1M2FlN2lsc0NkekNIMG1PODVXS2ttYWI0Y2tBN295TjI3V2VjQ21Hck5DdHcrOTA4Zm85c2g0RE91NTJabXRZajJNZTRQaUNnS0lqOVwvdkN4TTdOVDdocGh1WTVHa0pQS2xrZXJlcVNTdmNiQnZWR0tcL0J3czdDUGg3RWptSDdoXC9LZUFFRHpTZlwvalBDQVJEcVd1dWg2bGRFRGQxN05XbnNsRk5EcFFZKzBpdGhER0dxQnJjd0IxcTQyNVVRUzROWG1zSVQ4cU9uRlMrUWtQSU9UbVJkVCtRVmNLV2p0Zjg1TT0iLCJ0aW1lc3RhbXAiOiIyMDI1LTAzLTEyVDAwOjU3OjE5WiJ9",
                     licenseCipherPath = "plc-kt-DEMO.bin"
+                )
+            ),
+            ContentInput(
+                "hls 2",
+                "TestRunner_HLS2",
+                "https://qa-api.blaklisted.com/api/v1/getSignedMaster?file=cmaf%2F9798874811662%2F9798874811662_33_35%2Fmaster.m3u8",
+                MimeTypes.APPLICATION_M3U8,
+                DrmConfigration(
+                    "N9NH",
+                    "eyJrZXlfcm90YXRpb24iOmZhbHNlLCJyZXNwb25zZV9mb3JtYXQiOiJvcmlnaW5hbCIsInVzZXJfaWQiOiJ0ZXN0X2RvdmUiLCJkcm1fdHlwZSI6IndpZGV2aW5lIiwic2l0ZV9pZCI6Ik45TkgiLCJoYXNoIjoiM0dsVFJLSng0QmJxUnJKb2NNOHQ3VCsrQmNQcEY0VkZhZ284dXkwUWJuVT0iLCJjaWQiOiI5Nzk4ODc0ODExNjYyXzMzXzM1XzY4ZDNhZGZhYTE2NTEiLCJwb2xpY3kiOiJidFl6NjNma0RcLzRUUExGa0FwRmM0TDhleDZtSCt3d1ZPc0JOYU85RHErOFRUZzlqZmJDUHZIeEYzSHZXQkpFbDVmKzlIOUNDXC9UZlA2em1XNTczQUxRQzFXNVZlUzVKbzVnMTFTa0pmYXZ0V0xkeFNmbmJzWEluenluZGpwV0FySEVxNkVKRnQySXJTUEZKaDJxakdQTSs4dENLcFY3NWo5UjdJbFY4Ymp1bTJKYlQwSzB3T0FWUzBWb3NWRVhPR2FpMGUwd1JXZEZaRkRqTDZrR0hwcktqVmJkdjJvbGd0YVVZK05SbndqbFZhSDg4OVwvTTR6MzRDZkVsUjgzUnNpIiwidGltZXN0YW1wIjoiMjAyNS0xMS0wNlQwMTowOTowNFoifQ=="
                 )
             )
         )
@@ -103,6 +117,7 @@ class ObjectSingleton {
             val data = com.doverunner.widevine.model.ContentData(
                 contentId = input.contentId,
                 url = input.url,
+                mimeType = input.mimeType,
                 localFileUrl = input.localFileUrl,
                 drmConfig = input.drmConfig
             )
--- samples/castsample/app/build.gradle.kts
+++ samples/castsample/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("com.doverunner:widevine:4.5.0")
+    implementation("com.doverunner:widevine:4.5.1")
     implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
     implementation(fileTree(mapOf("include" to listOf("*.aar"), "dir" to "libs")))
 
